eee91d8a15d9a702b9c1536fd759abd32a5da632
margaretpalma <mpalma1496@gmail.com>
2025-10-01 21:43:16 -0400

    added some code

--- src/main/java/com/pluralsight/AddressBuilder.java
+++ src/main/java/com/pluralsight/AddressBuilder.java
@@ -1,7 +1,7 @@
 package com.pluralsight;
 import java.util.Scanner;
 public class AddressBuilder {
-    private static void main(String[] args) {
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         StringBuilder addressInfo = new StringBuilder();
@@ -11,13 +11,32 @@ public class AddressBuilder {
         //billing address
         //shipping address
 
-        System.out.println("Enter Full Name: ");
-        String fullName = scanner.nextln();
-        addressInfo.append(fullName).append("\n");
+        System.out.print("Enter your full name: ");
+        String fullName = scanner.nextLine();
 
-        addressInfo.append("Billing Address: \n");
-        System.out.print("Billing Street: ");
+        System.out.print("Billing street: ");
+        String billingStreet = scanner.nextLine();
+        System.out.print("Billing City: ");
+        String billingCity = scanner.nextLine();
+        System.out.print("Billing State: ");
+        String billingState = scanner.nextLine();
+        System.out.print("Billing Zipcode: ");
+        String billingZipcode = scanner.nextLine();
 
+        System.out.print("Shipping street: ");
+        String shippingStreet = scanner.nextLine();
+        System.out.print("Shipping City: ");
+        String shippingCity = scanner.nextLine();
+        System.out.print("Shipping State: ");
+        String shippingState = scanner.nextLine();
+        System.out.print("Shipping Zipcode: ");
+        String shippingZipcode = scanner.nextLine();
+
+       addressInfo.append(fullName).append("\n")
+               .append("Billing Address:\n")
+               .append(billingStreet).append("\n")
+               .append(billingCity).append(",").append(billingState)
+               .append(billingStreet).append("\n")
 
     }
 }
